10f1a15912552f05f26449c1ae0bd354c5a198f9
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2023-10-16 18:40:18 -0600

    SE AGREGO NUESTRA PROPIA IMPLEMENTACION DE USER DETAILS SERVICE

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -4,12 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,7 +28,7 @@ public class SecurityConfig {
 
         return http.build();
     }
-
+/*  crear usuarios en memoria
     @Bean
     public UserDetailsService memoryUser(){
         UserDetails admin = User.builder()
@@ -51,7 +47,7 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(admin, customer);
 
     }
-
+*/
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
